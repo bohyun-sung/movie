56678171b2cc0c76f2fbc2079dc4a47f9cf0239d
성보현 <djajsl8799@naver.com>
2026-03-23 23:55:38 +0900

    chore: validation message 생성

---
+++ movie/src/main/java/com/toyproject/movie/common/constants/ValidationMessage.java
@@ -0,0 +1,15 @@
+package com.toyproject.movie.common.constants;
+
+import com.toyproject.movie.common.exception.ServerException;
+import com.toyproject.movie.global.enums.ExceptionType;
+
+public final class ValidationMessage {
+
+    private ValidationMessage() {
+        throw new ServerException(ExceptionType.INTERNAL_SERVER_ERROR, "");
+    }
+
+    public static final String SEAT_NOT_EMPTY = "좌석을 선택해주세요.";
+    public static final String SCHEDULE_NOT_NULL = "상영관을 선택해주세요.";
+    public static final String CLIENT_NOT_NULL = "고객 IDX를 입력해주세요.";
+}
