40d30d3689ccb98de965f9d741ed6cd16e92d218
성보현 <djajsl8799@naver.com>
2026-03-25 01:38:44 +0900

    fix:  NotBlank -> NotNull

--- movie/src/main/java/com/toyproject/movie/api/dto/movie/request/MovieCreateReq.java
+++ movie/src/main/java/com/toyproject/movie/api/dto/movie/request/MovieCreateReq.java
@@ -1,10 +1,10 @@
 package com.toyproject.movie.api.dto.movie.request;
 
-import com.toyproject.movie.common.constants.ValidationMessage;
 import com.toyproject.movie.core.domain.movie.Movie;
 import com.toyproject.movie.global.enums.AudienceType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -18,15 +18,15 @@ public record MovieCreateReq(
         String movieTitle,
 
         @Schema(description = "상영 시간", example = "02:28:00")
-        @NotBlank(message = RUNTIME_NOT_BLANK)
+        @NotNull(message = RUNTIME_NOT_NULL)
         LocalTime runtime,
 
         @Schema(description = "관람 등급 (ALL, TWELVE, FIFTEEN, EIGHTEEN)", example = "ALL")
-        @NotBlank(message = AUDIENCE_NOT_NULL)
+        @NotNull(message = AUDIENCE_NOT_NULL)
         AudienceType audienceType,
 
         @Schema(description = "개봉일", example = "2024-03-24")
-        @NotBlank(message = RELEASE_DATE_NOT_BLANK)
+        @NotNull(message = RELEASE_DATE_NOT_NULL)
         LocalDate releaseDate
 ) {
     public Movie toEntity() {
