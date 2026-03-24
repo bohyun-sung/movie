e9becc54afa08ffc858d9ef0e504108b2e36b6e8
성보현 <djajsl8799@naver.com>
2026-03-25 01:22:32 +0900

    feat: 상영관 지점명 상영관명 중복 검증 추가

--- movie/src/main/java/com/toyproject/movie/core/repository/theater/TheaterRepository.java
+++ movie/src/main/java/com/toyproject/movie/core/repository/theater/TheaterRepository.java
@@ -4,4 +4,5 @@ import com.toyproject.movie.core.domain.theater.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
+    boolean existsByCinemaNameAndTheaterName(String cinemaName, String theaterName);
 }
--- movie/src/main/java/com/toyproject/movie/core/service/thearter/TheaterService.java
+++ movie/src/main/java/com/toyproject/movie/core/service/thearter/TheaterService.java
@@ -1,10 +1,12 @@
 package com.toyproject.movie.core.service.thearter;
 
 import com.toyproject.movie.api.dto.theater.request.TheaterCreateReq;
+import com.toyproject.movie.common.exception.ClientException;
 import com.toyproject.movie.core.domain.theater.Seat;
 import com.toyproject.movie.core.domain.theater.Theater;
 import com.toyproject.movie.core.repository.theater.SeatRepository;
 import com.toyproject.movie.core.repository.theater.TheaterRepository;
+import com.toyproject.movie.global.enums.ExceptionType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,10 @@ public class TheaterService {
 
     @Transactional
     public void createTheater(TheaterCreateReq req) {
+        // 상영관 이름 중복
+        if (theaterRepository.existsByCinemaNameAndTheaterName(req.cinemaName(), req.theaterName())) {
+            throw new ClientException(ExceptionType.CONFLICT, "이미 해당 영화관에 동일한 이름의 상영관이 존재합니다.");
+        }
         // 행 좌석
         char row = req.lastSeatRow().toUpperCase(Locale.ROOT).charAt(0);
         List<String> totalSeatRow = IntStream.rangeClosed('A', row)
@@ -45,6 +51,7 @@ public class TheaterService {
                 .flatMap(seatRow -> totalSeatColum.stream()
                         .map(seatColum -> Seat.of(seatRow, seatColum, saveTheater)))
                 .toList();
+
         seatRepository.saveAll(seatList);
     }
 }
