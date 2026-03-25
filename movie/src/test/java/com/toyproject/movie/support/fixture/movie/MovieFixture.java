2b2e09830cc15e3861ab66dfaee6028d76a8000c
성보현 <djajsl8799@naver.com>
2026-03-25 23:40:36 +0900

    chore: MovieFixture | ScheduleCreateReqFixture 생성

---
+++ movie/src/test/java/com/toyproject/movie/support/fixture/movie/MovieFixture.java
@@ -0,0 +1,20 @@
+package com.toyproject.movie.support.fixture.movie;
+
+import com.toyproject.movie.core.domain.movie.Movie;
+import com.toyproject.movie.global.enums.AudienceType;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class MovieFixture {
+    public static Movie createDefault() {
+        return Movie.of(
+                "재밋는 영화",
+                LocalTime.of(2,30),
+                AudienceType.FIFTEEN,
+                0,
+                LocalDate.of(2026,3,25),
+                false
+        );
+    }
+}
