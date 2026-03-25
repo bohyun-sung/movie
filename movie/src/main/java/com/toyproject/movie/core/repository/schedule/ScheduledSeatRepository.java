1bd4381b5cd782193fa2726fe5337cc4475433a2
성보현 <djajsl8799@naver.com>
2026-03-26 05:41:34 +0900

    feat: 비관적 락 findAllByIdWithLock

--- movie/src/main/java/com/toyproject/movie/core/repository/schedule/ScheduledSeatRepository.java
+++ movie/src/main/java/com/toyproject/movie/core/repository/schedule/ScheduledSeatRepository.java
@@ -1,7 +1,17 @@
 package com.toyproject.movie.core.repository.schedule;
 
 import com.toyproject.movie.core.domain.schedule.ScheduledSeat;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ScheduledSeatRepository extends JpaRepository<ScheduledSeat, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ss from ScheduledSeat ss where ss.ssIdx in :ssIdxs")
+    List<ScheduledSeat> findAllByIdWithLock(@Param("ssIdxs") List<Long> ssIdxs);
 }
