c58622d82967557f1575edb97793b38a49cceb3d
성보현 <djajsl8799@naver.com>
2026-03-26 05:44:31 +0900

    feat: findAllByTheaterIdx 추가

--- movie/src/main/java/com/toyproject/movie/core/repository/theater/SeatRepository.java
+++ movie/src/main/java/com/toyproject/movie/core/repository/theater/SeatRepository.java
@@ -2,6 +2,13 @@ package com.toyproject.movie.core.repository.theater;
 
 import com.toyproject.movie.core.domain.theater.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    @Query("SELECT s FROM Seat s " +
+            "WHERE s.theater.theaterIdx = :theaterIdx")
+    List<Seat> findAllByTheaterIdx(Long theaterIdx);
 }
