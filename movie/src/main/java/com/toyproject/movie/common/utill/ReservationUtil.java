db42dffcae31aadeea700a47fd7b977e12155f2c
성보현 <djajsl8799@naver.com>
2026-03-26 01:29:49 +0900

    refactor: Double 안정성을 위해 Math.round 추가

--- movie/src/main/java/com/toyproject/movie/common/utill/ReservationUtil.java
+++ movie/src/main/java/com/toyproject/movie/common/utill/ReservationUtil.java
@@ -4,9 +4,13 @@ import com.toyproject.movie.global.enums.AudienceDiscountType;
 
 public class ReservationUtil {
 
+    /**
+     * 할인된 티켓에 관람객의 연령에 따라 추가 할인 적용
+     * @param discountTicketPrice  할인된 티켓가격  [현재 조조 할인 만 존재]
+     * @param audienceDiscountType 관람객 연령
+     */
     public static Integer totalDiscount(Integer discountTicketPrice, AudienceDiscountType audienceDiscountType) {
-        return (int) (discountTicketPrice * (1 - audienceDiscountType.getDiscount()));
+        return (int) Math.round(discountTicketPrice * (1 - audienceDiscountType.getDiscount()));
     }
-
 
 }
