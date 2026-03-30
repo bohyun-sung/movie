72f6645f83f3a8eff04b541cc4b8de9c2870b54a
성보현 <djajsl8799@naver.com>
2026-03-31 01:22:17 +0900

    chore: Outbox entity 생성

---
+++ movie/src/main/java/com/toyproject/movie/core/domain/outbox/Outbox.java
@@ -0,0 +1,35 @@
+package com.toyproject.movie.core.domain.outbox;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "outbox")
+public class Outbox {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "aggregate_type", comment = "ex) MOVIE, RESERVATION")
+    private String aggregateType;
+    @Column(name = "aggregate_id", comment = "해당 도메인의 PK")
+    private Long aggregateId;
+    @Column(name = "event_type", comment = "ex) RESERVATION_CREATED")
+    private String eventType;
+    @Column(columnDefinition = "TEXT", comment = "실제 데이터 (JSON String)")
+    private String payload;
+
+
+    private Outbox(String aggregateType, Long aggregateId, String eventType, String payload) {
+        this.aggregateType = aggregateType;
+        this.aggregateId = aggregateId;
+        this.eventType = eventType;
+        this.payload = payload;
+    }
+
+    public Outbox of(String aggregateType, Long aggregateId, String eventType, String payload) {
+        return new Outbox(aggregateType, aggregateId, eventType, payload);
+    }
+}
